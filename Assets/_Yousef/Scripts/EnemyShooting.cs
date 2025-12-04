34f38ad3ff16d86dc6435476c67e8dac21b6c391
Yuzu1999 <63368986+YousefAlmikainzi@users.noreply.github.com>
2025-12-04 17:25:15 +0300

    shooting

--- Assets/_Yousef/Scripts/EnemyShooting.cs
+++ Assets/_Yousef/Scripts/EnemyShooting.cs
@@ -4,15 +4,46 @@ public class EnemyShooting : MonoBehaviour
 {
     [SerializeField] GameObject shot;
     [SerializeField] float shotCoolDown = 1;
-
+    [SerializeField] float bulletHeight = 0;
+    [SerializeField] float howFurther = 0;
+    
     float nextShot = 0;
+    Vector3 shotPosition;
 
     void Update()
     {
         if(Input.GetMouseButton(0) && Time.time >= nextShot)
         {
             nextShot = Time.time + shotCoolDown;
-            GameObject go = Instantiate(shot, transform.position, transform.rotation);
+            shotPosition= transform.position;
+            shotPosition.y = shotPosition.y + bulletHeight;
+            shotPosition.x = shotPosition.x - 2.0f;
+            shotPosition.z = shotPosition.z + howFurther;
+
+            GameObject go = Instantiate(shot, shotPosition, transform.rotation);
+
+            EnemyProjectile proj = go.GetComponent<EnemyProjectile>();
+        }
+        if (Input.GetMouseButton(2) && Time.time >= nextShot)
+        {
+            nextShot = Time.time + shotCoolDown;
+            shotPosition = transform.position;
+            shotPosition.y = shotPosition.y + bulletHeight;
+            shotPosition.z = shotPosition.z + howFurther;
+
+            GameObject go = Instantiate(shot, shotPosition, transform.rotation);
+
+            EnemyProjectile proj = go.GetComponent<EnemyProjectile>();
+        }
+        if (Input.GetMouseButton(1) && Time.time >= nextShot)
+        {
+            nextShot = Time.time + shotCoolDown;
+            shotPosition = transform.position;
+            shotPosition.y = shotPosition.y + bulletHeight;
+            shotPosition.x = shotPosition.x + 2.0f;
+            shotPosition.z = shotPosition.z + howFurther;
+
+            GameObject go = Instantiate(shot, shotPosition, transform.rotation);
 
             EnemyProjectile proj = go.GetComponent<EnemyProjectile>();
         }
